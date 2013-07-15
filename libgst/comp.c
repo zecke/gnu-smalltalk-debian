4dc738da604d48a2c51813f2cbb0f2063cbc5c38
Gwenael Casaccio <mrgwen@gmail.com>
2013-07-15 20:18:48 +0200

    I hope it's better

--- libgst/comp.c
+++ libgst/comp.c
@@ -2200,7 +2200,7 @@ _gst_make_constant_oop (tree_node constExpr)
 
       //incPtr = INC_SAVE_POINTER ();
       result = instantiate_with (_gst_array_class, len, &resultOOP);
-      _gst_register_oop (resultOOP);
+      INC_ADD_OOP (resultOOP); //_gst_register_oop (resultOOP);
 
       for (i = 0, subexpr = constExpr->v_const.val.aVal; i < len;
 	   i++, subexpr = subexpr->v_list.next)
@@ -2376,7 +2376,7 @@ add_literal (OOP oop)
 
   i =_gst_compiler_state->literal_vec_curr - _gst_compiler_state->literal_vec;
   *_gst_compiler_state->literal_vec_curr++ = oop;
-  _gst_register_oop (oop);
+  INC_ADD_OOP (oop); //_gst_register_oop (oop);
   return i;
 }
 
